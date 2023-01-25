32df6090d6d2df36bca61be5372dd3cd2e3c59e7
Emmanuel Ochoa <eorivera95@gmail.com>
2023-01-25 11:14:11 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <header>TS Amazona</header>
+        <main></main>
+        <footer>All rights reserved</footer>
+      </div>
+    </div>
+  )
+}
+
+export default App
